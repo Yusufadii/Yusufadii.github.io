0b1e5369906ce06231c83f32f9ee9817ee9d403b
Yusufadii <yusufadi960@gmail.com>
2023-02-17 10:31:51 +0700

    update

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 /*=============== SHOW MENU ===============*/
-const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
+const navMenu = document.getElementById('nav-menu')
+const navToggle = document.getElementById('nav-toggle')
 
 /*===== MENU SHOW =====*/
 /* Validate If Constant Exists */
