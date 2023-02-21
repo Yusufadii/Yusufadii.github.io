257d4698c3fad36602b48db772952ea23d70a2e2
Yusufadii <yusufadi960@gmail.com>
2023-02-21 16:51:37 +0700

    update icon

--- script.js
+++ script.js
@@ -6,14 +6,6 @@ navToggle.addEventListener("click", function() {
     navMenu.classList.toggle("show-menu");
 }) 
 
-var el = document.getElementById('nav-toggle');
-el.addEventListener('click', function() {
-	var klass = this.className;
-  if (klass === "") {
-      this.setAttribute('class', 'spin-right');
-    } else if (klass === 'spin-right') {
-      this.setAttribute('class', 'spin-left');
-    } else {
-      this.setAttribute('class', 'spin-right');
-    }
-}, false);
+$(".rotate").click(function(){
+  $(this).toggleClass("down"); 
+})
